456e16a64a2985237bc003be8a7e7899653e28a0
Marina Sessa <marinasessa1995@gmail.com>
2021-11-16 10:12:58 +0100

    Ho creato la view journal con view annesse

---
+++ Shared/Journal/HistoryView.swift
@@ -0,0 +1,25 @@
+//
+//  HistoryView.swift
+//  afterus
+//
+//  Created by Marina Sessa on 15/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct HistoryView: View{
+    var body: some View{
+        NavigationView{
+            VStack {
+        Text("Sergiy")
+                    .padding(.horizontal)
+                    .navigationTitle("HISTORY")
+            Image("LogoHistoryView")
+            
+            }
+    }
+    }
+    
+    
+}
--- Shared/Journal/JournalView.swift
+++ Shared/Journal/JournalView.swift
@@ -8,13 +8,53 @@
 import Foundation
 import SwiftUI
 
-
 struct JournalView: View {
     var body: some View {
-        Text("naggiamaronn")
-        //NavigationView {
-        //    VStack{
-        //    }
-        //}
+        NavigationView {
+            VStack {
+                Spacer()
+                NavigationLink(destination: NewNoteView()) {
+                    makeButtonView(title: "New note")
+                }
+                Spacer()
+                NavigationLink(destination: HistoryView()) {
+                    makeButtonView(title: "History")
+                }
+                Spacer()
+                NavigationLink(destination: CheckView()) {
+                    makeButtonView(title: "Check")
+                }
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image("LogoJournalView")
+                        .padding()
+                }
+            }
+            .padding(.horizontal)
+            .navigationTitle("JOURNAL")
+            
+            .toolbar{
+                Button {
+                } label: {
+                    NavigationLink(destination: ProfileView()){
+                        Image(systemName:"person.crop.circle")
+                    }
+                    
+                }
+            }
+        }
     }
+    
+}
+
+
+func makeButtonView(title: String) -> some View {
+    Text(title)
+        .bold()
+        .padding()
+        .frame(maxWidth: .infinity, minHeight: 60)
+        .foregroundColor(.black)
+        .background(Color(uiColor: .init(red: 224 / 255, green: 153 / 255, blue: 121 / 255, alpha: 1)))
+        .cornerRadius(14)
 }

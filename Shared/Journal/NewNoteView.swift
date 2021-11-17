311996af8abb513c7f7443ac701819685a46bf09
Marina Sessa <marinasessa1995@gmail.com>
2021-11-17 09:44:43 +0100

    hbjhnh

--- Shared/Journal/NewNoteView.swift
+++ Shared/Journal/NewNoteView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 
 struct NewNoteView: View{
-    @State private var testo = "Enter your feelings"
+    @State private var testo = ""
     @State private var testo1 = ""
     @State private var testo2 = ""
     @State private var search = ""
@@ -22,6 +22,7 @@ struct NewNoteView: View{
                 Text(" \(search)")
                 .searchable(text: $search)
                 .navigationTitle("NEW NOTE")
+                .padding()
                 VStack{
                     TextField("Enter your surname", text: $testo2)
                         .padding([.top, .leading, .trailing], 15.0)
@@ -78,11 +79,11 @@ struct NewNoteView_Previews: PreviewProvider {
         
     }
 }
-//
-//struct TextEditingView: View {
-//    @State private var fullText: String = "Write here your feelings about today..."
-//
-//    var body: some View {
-//        TextEditor(text: $fullText)
-//    }
-//}
+
+struct TextEditingView: View {
+    @State private var fullText: String = "Write here your feelings about today..."
+
+    var body: some View {
+        TextEditor(text: $fullText)
+    }
+}
